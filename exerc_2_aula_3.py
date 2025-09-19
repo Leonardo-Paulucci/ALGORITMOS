f38c65588cc41bf8e08243cf300cfb13064ae05c
Leonardo-Paulucci <leopauluccidias@gmail.com>
2025-09-18 21:44:03 -0300

    Delete exerc_2_aula_3.py

--- exerc_2_aula_3.py
+++
@@ -1,6 +0,0 @@
-aluno = {"nome": "Leonardo", "idade": 29, "curso": "Engenharia de Software"}
-print("Nome do aluno:", aluno["nome"])
-
-aluno["nota"] = 9.5   
-del aluno["idade"]   
-print("Dicionário aluno após alterações:", aluno)
