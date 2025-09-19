7b14accd65066972786390911a50cbc95b1ccbd8
Leonardo-Paulucci <leopauluccidias@gmail.com>
2025-09-18 22:00:06 -0300

    Add files via upload

---
+++ EXERC_AULA3_ALG/exerc_5_aula_3.py
@@ -0,0 +1,8 @@
+turma = {
+    "aluno1": {"nome": "João", "nota": 7},
+    "aluno2": {"nome": "Maria", "nota": 9}
+}
+
+print("Nome do primeiro aluno:", turma["aluno1"]["nome"])
+
+print("Nota do segundo aluno:", turma["aluno2"]["nota"])
