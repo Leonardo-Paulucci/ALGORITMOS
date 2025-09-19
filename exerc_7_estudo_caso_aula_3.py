d1b891f30f1c1377779b9cb57df4a929736f1e6b
Leonardo-Paulucci <leopauluccidias@gmail.com>
2025-09-18 21:44:38 -0300

    Delete exerc_7_estudo_caso_aula_3.py

--- exerc_7_estudo_caso_aula_3.py
+++
@@ -1,8 +0,0 @@
-produto1 = {"nome": "Arroz", "preco": 25.90, "estoque": 100}
-produto2 = {"nome": "Feijão", "preco": 12.50, "estoque": 50}
-produto3 = {"nome": "Macarrão", "preco": 8.30, "estoque": 80}
-
-produtos = [produto1, produto2, produto3]
-
-for produto in produtos:
-    print(f"O produto {produto['nome']} custa R${produto['preco']} e tem {produto['estoque']} unidades em estoque.")
